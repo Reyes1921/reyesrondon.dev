f4c8aad676e351cdd91454b039e9f5083cb4681d
Reyes Rondon <reyes192119@gmail.com>
2024-06-03 17:40:31 -0400

    update skills

--- src/app/components/skills/Skills.tsx
+++ src/app/components/skills/Skills.tsx
@@ -1,3 +1,8 @@
+"use client"
+
+import Atropos from "atropos/react"
+import "atropos/css"
+
 export const Skills = () => {
   const frontend = [
     {
@@ -37,7 +42,7 @@ export const Skills = () => {
     },
     {
       path: "/icons/bootstrap.svg",
-      title: "Bootstrap CSS",
+      title: "Bootstrap",
       alt: "Bootstrap logo",
     },
   ]
@@ -125,7 +130,7 @@ export const Skills = () => {
   ]
 
   return (
-    <article className="flex items-center justify-center flex-col-reverse md:flex-row">
+    <article className="flex items-center justify-center  md:flex-row">
       <div className="p-10 md:pr-10 w-full">
         <h2
           id="projects"
@@ -135,95 +140,116 @@ export const Skills = () => {
         </h2>
 
         <div className="">
-          <div className="">
-            <h2 className="text-center text-3xl font-bold mb-4 mt-5">
-              Frontend
-            </h2>
-            <div className="flex flex-row justify-between items-center backdrop-brightness-75">
+          <h2 className="text-center text-lime-600 text-3xl font-bold mb-4 mt-5">
+            Frontend
+          </h2>
+          <Atropos className="my-atropos mt-5">
+            <div className="grid md:flex grid-cols-3 md:flex-row justify-around items-center backdrop-brightness-75 border border-lime-600 rounded-xl">
               {frontend.map((icon) => (
-                <div className="flex flex-col flex-justify spac- items-center p-4">
+                <div
+                  key={icon.title}
+                  className="flex flex-col flex-justify items-center p-4"
+                >
                   <img
                     src={icon.path}
-                    className="w-11 md:w-10 color-[#F7DF1E]"
+                    className="w-7 md:w-10 color-[#F7DF1E]"
                     alt={icon.alt}
                   />
-                  <span className="text-sm mt-2">{icon.title}</span>
+                  <span className="text-xs mt-2">{icon.title}</span>
                 </div>
               ))}
             </div>
-          </div>
-          <div className="flex flex-row justify-around">
+          </Atropos>
+
+          <div className="flex flex-col md:flex-row justify-between">
             <div>
-              <h2 className="text-center text-3xl font-bold mb-4 mt-5 pr-2">
+              <h2 className="text-center text-lime-600 text-3xl font-bold mb-4 mt-5 pr-2">
                 Backend
               </h2>
-              <div className="flex flex-row justify-between items-center backdrop-brightness-75">
-                {backend.map((icon) => (
-                  <div className="flex flex-col flex-justify items-center p-4">
-                    <img
-                      src={icon.path}
-                      className="w-10 md:w-10 color-[#F7DF1E]"
-                      alt={icon.alt}
-                    />
-                    <span className="text-sm mt-2">{icon.title}</span>
-                  </div>
-                ))}
-              </div>
+              <Atropos className="my-atropos mt-5 ">
+                <div className="flex flex-row justify-around md:justify-between items-center backdrop-brightness-75 border border-lime-600 rounded-xl">
+                  {backend.map((icon) => (
+                    <div
+                      key={icon.title}
+                      className="flex flex-col flex-justify items-center p-4"
+                    >
+                      <img
+                        src={icon.path}
+                        className="w-7 md:w-10 color-[#F7DF1E]"
+                        alt={icon.alt}
+                      />
+                      <span className="text-xs mt-2">{icon.title}</span>
+                    </div>
+                  ))}
+                </div>
+              </Atropos>
             </div>
             <div>
-              <h2 className="text-center text-3xl font-bold mb-4 mt-5">
+              <h2 className="text-center text-lime-600 text-3xl font-bold mb-4 mt-5">
                 Bases de Datos
               </h2>
-              <div className="flex flex-row justify-between items-center backdrop-brightness-75">
-                {bd.map((icon) => (
-                  <div className="flex flex-col flex-justify items-center p-4">
-                    <img
-                      src={icon.path}
-                      className="w-10 md:w-10 color-[#F7DF1E]"
-                      alt={icon.alt}
-                    />
-                    <span className="text-sm mt-2">{icon.title}</span>
-                  </div>
-                ))}
-              </div>
+              <Atropos className="my-atropos mt-5">
+                <div className="flex flex-row justify-around md:justify-between items-center backdrop-brightness-75 border border-lime-600 rounded-xl">
+                  {bd.map((icon) => (
+                    <div
+                      key={icon.title}
+                      className="flex flex-col flex-justify items-center p-4"
+                    >
+                      <img
+                        src={icon.path}
+                        className="w-7 md:w-10 color-[#F7DF1E]"
+                        alt={icon.alt}
+                      />
+                      <span className="text-xs mt-2">{icon.title}</span>
+                    </div>
+                  ))}
+                </div>
+              </Atropos>
             </div>
             <div>
-              {" "}
-              <h2 className="text-center text-3xl font-bold mb-4 mt-5 pr-2">
+              <h2 className="text-center text-lime-600 text-3xl font-bold mb-4 mt-5 pr-2">
                 Testing
               </h2>
-              <div className="flex flex-row justify-center items-center backdrop-brightness-75">
-                {testing.map((icon) => (
-                  <div className="flex flex-col flex-justify items-center p-4">
-                    <img
-                      src={icon.path}
-                      className="w-10 md:w-10 color-[#F7DF1E]"
-                      alt={icon.alt}
-                    />
-                    <span className="text-sm mt-2">{icon.title}</span>
-                  </div>
-                ))}
-              </div>
+              <Atropos className="my-atropos mt-5">
+                <div className="flex flex-row justify-around items-center backdrop-brightness-75 border border-lime-600 rounded-xl">
+                  {testing.map((icon) => (
+                    <div
+                      key={icon.title}
+                      className="flex flex-col flex-justify items-center p-4"
+                    >
+                      <img
+                        src={icon.path}
+                        className="w-7 md:w-10 color-[#F7DF1E]"
+                        alt={icon.alt}
+                      />
+                      <span className="text-xs mt-2">{icon.title}</span>
+                    </div>
+                  ))}
+                </div>
+              </Atropos>
             </div>
           </div>
-          <div className=""></div>
-          <div className="">
-            <h2 className="text-center text-3xl font-bold mb-4 mt-5 pr-2">
-              Herramientas
-            </h2>
-            <div className="flex flex-row justify-between items-center backdrop-brightness-75">
+
+          <h2 className="text-center text-lime-600 text-3xl font-bold mb-4 mt-5 pr-2">
+            Herramientas
+          </h2>
+          <Atropos className="my-atropos mt-5">
+            <div className="grid md:flex grid-cols-3 md:flex-row justify-between items-center backdrop-brightness-75 border border-lime-600 rounded-xl">
               {tools.map((icon) => (
-                <div className="flex flex-col flex-justify items-center p-4">
+                <div
+                  key={icon.title}
+                  className="flex flex-col flex-justify items-center p-4"
+                >
                   <img
                     src={icon.path}
-                    className="w-10 md:w-10 color-[#F7DF1E]"
+                    className="w-7 md:w-10 color-[#F7DF1E]"
                     alt={icon.alt}
                   />
-                  <span className="text-sm mt-2">{icon.title}</span>
+                  <span className="text-xs mt-2">{icon.title}</span>
                 </div>
               ))}
             </div>
-          </div>
+          </Atropos>
         </div>
       </div>
     </article>
