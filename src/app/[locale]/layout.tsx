d061d769cead9849802da90aed795c33989d87c7
Reyes Rondon <reyes192119@gmail.com>
2025-06-08 19:20:20 -0400

    google analytics

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -4,7 +4,7 @@ import {getMessages, getTranslations} from "next-intl/server"
 import {cookies} from "next/headers"
 import {Footer, Header} from "../components"
 import "./globals.css"
-import {GoogleTagManager} from "@next/third-parties/google"
+import {GoogleTagManager, GoogleAnalytics} from "@next/third-parties/google"
 interface RootMetadata {
   params: {locale: string}
 }
@@ -71,6 +71,7 @@ export default async function RootLayout({
   return (
     <html lang={locale} className={theme}>
       <head>
+        <GoogleAnalytics gaId="G-BK2C3D0GKH" />
         <GoogleTagManager gtmId="GTM-TQFZ2GNB" />
         <script
           type="application/ld+json"
